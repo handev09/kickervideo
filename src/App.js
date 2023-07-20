e58f0cd4d9dbc12a6daf032a56ee175544172a78
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2023-07-20 15:24:52 +0300

    added login page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterPage from './RegisterPage';
 import DashboardPage from './DashboardPage';
 import SignupPage from './SignUp';
+import LoginPage from './LoginPage';
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
   );
