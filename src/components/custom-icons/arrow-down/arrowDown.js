d89ab810e3b93063ce22b444195469cf2e4f0faa
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2023-07-27 22:26:52 +0300

    modified the add crew form

---
+++ src/components/custom-icons/arrow-down/arrowDown.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ArrowDown = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="48"
+      viewBox="0 -960 960 960"
+      width="48"
+    >
+      <path d="M480-345 240-585l43-43 197 198 197-197 43 43-240 239Z" />
+    </svg>
+  );
+};
+
+export default ArrowDown;
--- src/pages/AddCrewForm.js
+++ src/pages/AddCrewForm.js
@@ -1,9 +1,9 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 import {
   Typography,
   FormControlLabel,
-  Checkbox,
+  
   TextField,
   FormControl,
   InputLabel,
@@ -12,14 +12,26 @@ import {
   Select,
   MenuItem,
   Container,
-} from '@mui/material';
+  Divider
+} from "@mui/material";
+import ArrowDown from "../components/custom-icons/arrow-down/arrowDown";
 
 const AddCrewForm = () => {
-    const [employmentType, setEmploymentType] = useState('');
+  const [employmentType, setEmploymentType] = useState("");
 
-    const handleEmploymentTypeChange = (event) => {
-        setEmploymentType(event.target.value);
-      };
+  const handleEmploymentTypeChange = (event) => {
+    setEmploymentType(event.target.value);
+  };
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownOpen = () => {
+    setIsDropdownOpen(true);
+  };
+
+  const handleDropdownClose = () => {
+    setIsDropdownOpen(false);
+  };
   return (
     <form>
       {/* Title */}
@@ -32,80 +44,131 @@ const AddCrewForm = () => {
         Profile Details
       </Typography>
 
-      <FormControl component="fieldset" sx={{width: '100%'}}>
+      <FormControl component="fieldset" sx={{ width: "100%", marginBottom: '31px' }}>
         <RadioGroup
           aria-label="employmentType"
           name="employmentType"
           value={employmentType}
           onChange={handleEmploymentTypeChange}
         >
-          <Container sx={{ display: 'flex', justifyContent: 'space-between', gap: '20px' }}>
-            <Container sx={{border: '1px solid #000', p:5, borderRadius: '8px'}}>
-            <FormControlLabel
-              value="Full time"
-              control={<Radio />}
-              label="Full time"
-              sx={{ ml: 2, mr: 1, borderRadius: '8px' }}
-            />
+          <Container
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              gap: "20px",
+            }}
+          >
+            <Container
+              sx={{ border: "1px solid #000", p: 5, borderRadius: "8px" }}
+            >
+              <FormControlLabel
+                value="Full time"
+                control={<Radio />}
+                label="Full time"
+                sx={{ ml: 2, mr: 1, borderRadius: "8px" }}
+              />
             </Container>
 
-            <Container sx={{border: '1px solid #000', p:5, borderRadius: '8px', }}>
-            <FormControlLabel
-              value="Part time"
-              control={<Radio />}
-              label="Part time"
-              sx={{ mr: 1, borderRadius: '8px' }}
-            />
+            <Container
+              sx={{ border: "1px solid #000", p: 5, borderRadius: "8px" }}
+            >
+              <FormControlLabel
+                value="Part time"
+                control={<Radio />}
+                label="Part time"
+                sx={{ mr: 1, borderRadius: "8px" }}
+              />
             </Container>
 
-            <Container sx={{border: '1px solid #000', p:5, borderRadius: '8px'}}>
-            <FormControlLabel
-              value="Freelancer / Contractor"
-              control={<Radio />}
-              label="Freelancer / Contractor"
-              sx={{ borderRadius: '8px' }}
-            />
+            <Container
+              sx={{ border: "1px solid #000", p: 5, borderRadius: "8px" }}
+            >
+              <FormControlLabel
+                value="Freelancer / Contractor"
+                control={<Radio />}
+                label="Freelancer / Contractor"
+                sx={{ borderRadius: "8px" }}
+              />
             </Container>
           </Container>
         </RadioGroup>
       </FormControl>
 
+      <Divider sx={{mb: '25px', mt: '31px'}} />
+
       {/* Input Boxes */}
       <Container
         sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          flexDirection: 'row',
-          gap: '10px',
-        //   maxWidth: '800px',
-          marginBottom: '20px',
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          gap: "10px",
+          //   maxWidth: '800px',
+          marginBottom: "20px",
         }}
       >
-        <TextField label="Name" variant="outlined" margin="normal" sx={{width: '30%'}} />
-        <TextField label="Email" variant="outlined" margin="normal" sx={{width: '30%'}} />
-        <TextField label="Phone" variant="outlined" margin="normal" sx={{width: '30%'}} />
-        <TextField label="Address" variant="outlined" margin="normal" sx={{width: '30%'}} />
-        <TextField label="City" variant="outlined" margin="normal" sx={{width: '30%'}} />
-        <FormControl variant="outlined" sx={{width: '30%'}}>
-        <InputLabel>Position</InputLabel>
-        <Select label="Position">
-          <MenuItem value="Developer">Developer</MenuItem>
-          <MenuItem value="Designer">Designer</MenuItem>
-          <MenuItem value="Manager">Manager</MenuItem>
-        </Select>
-      </FormControl>
+        <TextField
+          label="Full Name"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "30%" }}
+        />
+        <TextField
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "30%" }}
+        />
+        <TextField
+          label="Phone"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "30%" }}
+        />
+        <TextField
+          label="Address"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "30%" }}
+        />
+        <TextField
+          label="Street"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "30%" }}
+        />
+
+<TextField
+          label="State / Province"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "30%" }}
+        />
+
+<TextField
+          label="City"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "30%" }}
+        />
+
+<TextField
+          label="Zip / Postal Code"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "30%" }}
+        />
       </Container>
 
-    
-  
-      <Container sx={{ display: 'flex', alignItems: 'center' }}>
+      <Container sx={{ display: "flex", alignItems: "center",  mt: 10}}>
         {/* ... Dropdown and input fields as before ... */}
-        <FormControl variant="outlined" style={{ marginRight: 20 }}>
-          <InputLabel>Dropdown</InputLabel>
+        <FormControl variant="outlined" style={{ marginRight: 20 }} sx={{width: '20%'}}>
+          <InputLabel>Rate</InputLabel>
           <Select label="Dropdown">
-            <MenuItem value="Option1">Option 1</MenuItem>
-            <MenuItem value="Option2">Option 2</MenuItem>
-            <MenuItem value="Option3">Option 3</MenuItem>
+            <MenuItem value="Option1">Hour</MenuItem>
+            <MenuItem value="Option2">Day</MenuItem>
+            <MenuItem value="Option3">Half Day</MenuItem>
+            <MenuItem value="Option3">Flat</MenuItem>
           </Select>
         </FormControl>
         <TextField
@@ -121,13 +184,9 @@ const AddCrewForm = () => {
           style={{ marginRight: 20 }}
         />
         <TextField label="Total" variant="outlined" margin="normal" />
-
       </Container>
     </form>
   );
 };
 
 export default AddCrewForm;
-
-
-
