90b897385331bf2eff45b2fe04e0ac1a5d7c25cf
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2023-10-03 08:27:10 +0300

    c

--- src/components/status-indicator/status.js
+++ src/components/status-indicator/status.js
@@ -7,9 +7,9 @@ const StateIndicator = ({ status }) => {
         sx={{
           padding: '10px', // Adjust the padding as needed
           backgroundColor:
-            status === 'active' ? '#4caf50' : // Green for Active state
-            status === 'draft' ? '#f44336' : // Red for Inactive state
-            status === 'Pending' ? '#ff9800' : // Orange for Pending state
+            status === 'Approved' ? '#4caf50' : // Green for Active state
+            status === 'Draft' ? '#f44336' : // Red for Inactive state
+            status === 'Awaiting Response' ? '#ff9800' : // Orange for Pending state
             '#000', // Default color if the status doesn't match any of the above
           color: '#fff', // Text color
           borderRadius: '4px', // Rounded corners
